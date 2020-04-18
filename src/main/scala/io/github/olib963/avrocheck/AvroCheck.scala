1574cf2c8ee657537c7cb2fa6626f2720a07fee3
Oliver Boyle <32272028+olib963@users.noreply.github.com>
2020-04-18 16:48:36 +0100

    Add some scaladoc

--- src/main/scala/io/github/olib963/avrocheck/AvroCheck.scala
+++ src/main/scala/io/github/olib963/avrocheck/AvroCheck.scala
@@ -12,25 +12,13 @@ import scala.reflect.ClassTag
 
 trait AvroCheck {
 
-  // Override definitions
-  val noOverrides: Overrides = NoOverrides
-  def overrideKeys(overrides: (String, Overrides)): Overrides = KeyOverrides(Map(overrides))
-
-  def overrideKeys(overrides: (String, Overrides), secondOverride: (String, Overrides), moreOverrides: (String, Overrides)*): Overrides =
-    KeyOverrides(Map(overrides +:secondOverride +: moreOverrides: _*))
-
-  def selectNamedUnion(branchName: String, overrides: Overrides = NoOverrides): Overrides = SelectedUnion(branchName, overrides)
-
-  def constantOverride[A](value: A): Overrides = ConstantOverride(value)
-  def generatorOverride[A: ClassTag](gen: Gen[A]): Overrides = GeneratorOverrides(gen)
-
-  def arrayOverride(elements: Seq[Overrides]): Overrides = ArrayOverrides(elements)
-  def arrayGenerationOverride(sizeGenerator: Gen[Int] = Gen.posNum[Int], elementOverrides: Overrides = NoOverrides): Overrides = ArrayGenerationOverrides(sizeGenerator, elementOverrides)
-
-  // Schema functions
-  def schemaFromResource(schemaResource: String): Schema =
-    new Schema.Parser().parse(Source.fromResource(schemaResource).mkString)
-
+  /**
+   * @param schema The avro schema to use to generate random values.
+   * @param configuration Allows configuration of default generation parameters e.g. the default [[Gen]] for [[String]] values.
+   * @param overrides Overrides can be used to customise the generation of arbitrarily nested values without affecting the
+   *                  generation of other values for example [[AvroCheck.overrideKeys]]
+   * @return A generator that will create a [[GenericRecord]] from the schema as long as the schema is a RECORD or UNION of RECORDs.
+   */
   def genFromSchema(schema: Schema, configuration: Configuration = Configuration.Default, overrides: Overrides = NoOverrides): Gen[GenericRecord] = schema.getType match {
     case Type.RECORD => recordGenerator(schema, configuration, overrides) match {
       case Right(gen) => gen
@@ -43,5 +31,98 @@ trait AvroCheck {
       }
     case _ => sys.error(s"Can only create generator for records or a union of records, schema is not supported: $schema")
   }
+
+  /** Utility function to parse a schema from a resource file. */
+  def schemaFromResource(schemaResource: String): Schema =
+    new Schema.Parser().parse(Source.fromResource(schemaResource).mkString)
+
+  /** The default override type - doesn't override any value generation and uses the defaults for the schema. */
+  val noOverrides: Overrides = NoOverrides
+
+  /**
+   * Ensure the same value is returned for every generation e.g. for a schema of type "int" you could pass constantOverride(10).
+   * The type of the value passed must mach the type generated for the schema.
+   */
+  def constantOverride[A](value: A): Overrides = ConstantOverride(value)
+
+  /**
+   * Customise the generator used to create a value for the schema. The type of the generator must match the type of the schema,
+   * for example for a schema of type "string" you might pass generatorOverride(Gen.alphaNumStr).
+   */
+  def generatorOverride[A: ClassTag](gen: Gen[A]): Overrides = GeneratorOverrides(gen)
+
+  /**
+   * Customise the generation of an array value for a schema of type "array".
+   *
+   * @param sizeGenerator generates the size of the array. This must be positive (currently not enforced so will cause errors)
+   * @param elementOverrides overrides to use for each element in the generated array.
+   */
+  def arrayGenerationOverride(sizeGenerator: Gen[Int] = Gen.posNum[Int], elementOverrides: Overrides = NoOverrides): Overrides = ArrayGenerationOverrides(sizeGenerator, elementOverrides)
+
+  /**
+   * Overrides array generation to create an array that:
+   * - Has the exact size of elements
+   * - Uses the overrides in elements in order to generate each element of the array.
+   */
+  def arrayOverride(elements: Seq[Overrides]): Overrides = ArrayOverrides(elements)
+
+  /**
+   * @see [[AvroCheck.overrideKeys]]
+   */
+  def overrideKeys(overrides: (String, Overrides)): Overrides = KeyOverrides(Map(overrides))
+
+  /**
+   *  For a schema of type "record" you can set an override for any field, any other fields will use default generation.
+   *  For example if your schema is:
+   *
+   *  {{{
+   *    {
+   *      "namespace": "example.avro",
+   *      "type": "record",
+   *      "name": "User",
+   *      "fields": [
+   *        {
+   *          "name": "name",
+   *          "type": "string"
+   *        },
+   *        {
+   *          "name": "age",
+   *          "type": "int"
+   *        },
+   *        {
+   *          "name": "favourite_number",
+   *          "type": [
+   *            "int",
+   *            "null"
+   *          ]
+   *        },
+   *        {
+   *          "name": "favourite_colour",
+   *          "type": [
+   *            "string",
+   *            "null"
+   *          ],
+   *          "default": "null"
+   *        }
+   *      ]
+   *    }
+   *  }}}
+   *
+   *  you would be able to pass.
+   *  {{{
+   *  overrideKeys("name" -> constantOverride("foo"), "age" -> generatorOverride(Gen.posNum[Int]))
+   *  }}}
+   */
+  def overrideKeys(overrides: (String, Overrides), secondOverride: (String, Overrides), moreOverrides: (String, Overrides)*): Overrides =
+    KeyOverrides(Map(overrides +:secondOverride +: moreOverrides: _*))
+
+  /**
+   * Allows to select which schema in a union of schemas to generate values for.
+   *
+   * @param branchName The name of the schema in the union to select, if the schema is a record then the name (with no namespace) of the record,
+   *                   if a primitive then the name of the primitive (e.g. "string")
+   * @param overrides The override values to use for the schema branch selected.
+   */
+  def selectNamedUnion(branchName: String, overrides: Overrides = NoOverrides): Overrides = SelectedUnion(branchName, overrides)
 
 }
